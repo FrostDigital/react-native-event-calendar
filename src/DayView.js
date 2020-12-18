1dfe64605ba17da1eb12a1826ceae8a89c267a2f
ishaniDolawatta <ishanihans@gmail.com>
2020-12-18 15:10:01 +0530

    set border color

--- src/DayView.js
+++ src/DayView.js
@@ -23,6 +23,9 @@ export default class DayView extends Component {
 				backgroundColor: event.color
 			};
 
+
+			const eventBorderColor = { borderColor: event.borderColor, borderWidth: 2 };
+
 			// Fixing the number of lines for the event title makes this calculation easier.
 			// However it would make sense to overflow the title to a new line if needed
 			const numberOfLines = Math.floor(event.height / TEXT_LINE_HEIGHT);
@@ -32,7 +35,7 @@ export default class DayView extends Component {
 					activeOpacity={0.5}
 					onPress={() => this._onEventTapped(event)}
 					key={i}
-					style={[styles.event, style, event.color && eventColor]}
+					style={[styles.event, style, event.color && eventColor, event.borderColor && eventBorderColor]}
 				>
 					{this.props.renderEvent ? (
 						this.props.renderEvent(event)
